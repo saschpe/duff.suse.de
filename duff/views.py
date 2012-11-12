ab4cbfb6c91911733f619fa398767d5591c845f3
Sascha Peilicke <saschpe@suse.de>
2012-11-12 23:02:00 +0100

    Initial index view and stub template

--- duff/views.py
+++ duff/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
 
+from duff.virtual.models import Domain
+
 
 def index(request):
-    return render_to_response('index.html')
+    domains = Domain.objects.all()
+    return render_to_response("index.html", {"domains": domains})
