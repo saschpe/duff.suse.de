6466a0ef5206c78763d15ee87fce5ba65877b0f1
Sascha Peilicke <saschpe@suse.de>
2012-11-16 19:58:48 +0100

    Rename management command to 'libvirt_update'

--- duff/virtual/management/commands/libvirt_update.py
+++ duff/virtual/management/commands/libvirt_update.py
@@ -7,4 +7,8 @@ class Command(BaseCommand):
     help="Update libvirt domains (virtual machines)"
 
     def handle(self, *args, **options):
+        conn = libvirt.openReadOnly(hypervisor)
+        Domain.objects.update_or_create_all_from_libvirt(conn)
+        Network.objects.update_or_create_all_from_libvirt(conn)
+        conn.close()
         Domain.objects.update_or_create_all_from_libvirt()
