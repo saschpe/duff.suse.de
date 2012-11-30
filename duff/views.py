394f4eff4605eb46a45df5fe78dbc91d3cd3ec78
Sascha Peilicke <saschpe@suse.de>
2012-11-30 20:47:47 +0100

    Add Apache-2.0 LICENSE file and add license headers

--- duff/views.py
+++ duff/views.py
@@ -1,12 +1,23 @@
-# -*- coding: utf-8 -*-
+# Copyright 2012 Sascha Peilicke <saschpe@gmx.de>
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 
 from django.shortcuts import render_to_response
 
-from duff.libvirt.models import Domain, Interface, Network
+from duff.libvirt.models import Domain, Network
 
 
 def index(request):
     domains = Domain.objects.all().prefetch_related("allocation_set")
-    #interfaces = Interface.objects.all()
     networks = Network.objects.all()
     return render_to_response("virt/index.html", {"domains": domains, "networks": networks})
