a0993e2aa046683355a972edf1da3fe1b157ce3b
Sascha Peilicke <saschpe@suse.de>
2012-11-30 22:42:49 +0100

    Add some inline admin models

--- duff/libvirt/admin.py
+++ duff/libvirt/admin.py
@@ -17,6 +17,16 @@ from django.contrib import admin
 from models import Allocation, Domain, Interface, Network, Service
 
 
+class AllocationInline(admin.TabularInline):
+    model = Allocation
+    extra = 0
+
+
+class InterfaceInline(admin.TabularInline):
+    model = Interface
+    extra = 0
+
+
 class AllocationAdmin(admin.ModelAdmin):
     list_display = ("domain", "service", "running")
     list_filter = ("domain", "service", "running")
@@ -27,6 +37,7 @@ class DomainAdmin(admin.ModelAdmin):
     list_display = ("name", "id", "state", "max_memory", "memory", "vcpus", "cpu_time")
     list_filter = ("state", "vcpus")
     search_fields = ("name", "state")
+    inlines = (AllocationInline, InterfaceInline)
 
 
 class InterfaceAdmin(admin.ModelAdmin):
@@ -39,12 +50,14 @@ class NetworkAdmin(admin.ModelAdmin):
     list_display = ("name", "bridge_name", "forward_mode", "domain_name", "active", "persistent") 
     list_filter = ("forward_mode", "domain_name", "active", "persistent")
     search_fields = ("name", "bridge_name", "domain_name")
+    inlines = (InterfaceInline,)
 
 
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ("name", "description", "port", "protocol")
     list_filter = ("port", "protocol")
     search_fields = ("name", "description")
+    inlines = (AllocationInline,)
 
 
 admin.site.register(Allocation, AllocationAdmin)
